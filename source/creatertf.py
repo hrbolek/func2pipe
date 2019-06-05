607477d808524739d9d02822ef54e39ac60fccf8
hrbolek <33178647+hrbolek@users.noreply.github.com>
2019-06-05 21:23:51 +0200

    Add files via upload

--- source/creatertf.py
+++ source/creatertf.py
@@ -3,15 +3,20 @@ import io
 import os
 import re
 
+from evaluator import compile_expression 
+
+
 def compile_template(template):
 
-    regexTester = re.compile(r'((?:\[\[)(?:[A-Z])(?:\:(?:[a-z_A-Z0-9]+))?(?:\((?:[^\)]+)\))?(?:\]\]))')
+    regexTester = re.compile(r'((?:\[\[)(?:[A-Z]+)(?:\:(?:[a-z_A-Z0-9]*))?(?:\((?:[^\)]*)\))?(?:\]\]))')
 #    last = 0
 #    for match in regexTester.finditer(template):
 #        yield match
     templatelist = regexTester.split(template)
+#    for item in templatelist:
+#        print(item[0:25])
 
-    regexTester = re.compile(r'(?:\[\[)([A-Z])(?:\:([a-z_A-Z0-9]+))?(?:\(([^\)]+)\))?(?:\]\])')
+    regexTester = re.compile(r'(?:\[\[)([A-Z]+)(?:\:([a-z_A-Z0-9]*))?(?:\(([^\)]*)\))?(?:\]\])')
 #    lnames = []
     types = []
     result = []
@@ -26,10 +31,10 @@ def compile_template(template):
 
     def uuesc(data):
         result = ''
-        for c in data:
+        for c in str(data):
             code = ord(c)
             if (code > 127):
-                result += '\\u' + str(code)+ 'G'
+                result += '\\u' + str(code)+ '?'
             else:
                 result += c
         return result
@@ -39,31 +44,36 @@ def compile_template(template):
             #print('softprinter enter', '|'+name+'|', str(data)[0:200])
             #print('softprinter enter', 'len', str(data[0])[0:200])
             #print(data[name].encode('unicode_escape').decode('ansi'))
-            printer(uuesc(data[name]))#.encode('unicode_escape').decode('ansi'))
+            if name in data:
+                printer(uuesc(data[name]))#.encode('unicode_escape').decode('ansi'))
+            else:
+                print('Error on field ', name)
 #            printer(data[name].encode('unicode_escape').decode('ansi'))
 #            print('softprinter leave')
             return data
         return result
 
-    def subroutine(funclist):
+    def subroutine(funclist, iflambda = lambda data: True):
         #print('subroutine on', str(funclist))
         def result(printer, data):
             #print('subroutine enter', str(data))
-            subdata = data
-            for f in funclist:
-                subdata = f(printer, subdata)
+            if (iflambda(data)):
+                subdata = data
+                for f in funclist:
+                    subdata = f(printer, subdata)
             #print('subroutine leave', str(data))
             return data
         return result
 
-    def iterator(funclist):
+    def iterator(funclist, iflambda = lambda data: True):
         #print('iterator on', str(funclist))
         def result(printer, data):
             #print('iterator enter', str(data))
             for item in data:
                 #print('iterator item', str(item))
-                for f in funclist:
-                    f(printer, item)
+                if (iflambda(item)):
+                    for f in funclist:
+                        f(printer, item)
             #print('iterator leave')
             return data
         return result
@@ -76,6 +86,41 @@ def compile_template(template):
             return data[name]
         return result
 
+    def filterit(filterfunc = lambda data: True):
+        def result(printer, data):
+            return filter(filterfunc, data)
+        return result
+
+    def agregate(itemname, funcname):
+        def sum(data):
+            result = 0
+            for item in data:
+                result += item[itemname]
+            return str(result)
+
+        def count(data):
+            result = 0
+            for item in data:
+                result += 1
+            return str(result)
+
+        func = sum
+        if (funcname == 'count'):
+            func = count
+
+        def result(printer, data):
+            value = func(data)
+            printer(value)
+        return result
+
+    def createtester(expression):
+        compiledexpr = compile_expression(expression)
+        def evaluate(data):
+            def callforvalue(name):
+                return data[name]
+            return compiledexpr(callforvalue)
+        return evaluate
+
     while (index < len(templatelist) - 1):
         index += 1
         item = templatelist[index]
@@ -85,13 +130,41 @@ def compile_template(template):
         elif (match.group(1) == 'W'):
             types += ['W']
             deep += 1
-            result.append((index, deep, 'W', match.group(2), select(match.group(2))))
+
+            iflambda = lambda data: True
+            if (not match.group(3) == None):
+                iflambda = createtester(match.group(3))
+            result.append((index, deep, 'W', match.group(2), select(match.group(2)), iflambda))
+
+        elif (match.group(1) == 'ITERATE'):
+            types += ['F']
+            deep += 1
+            result.append((index, deep, 'ITERATE', '', lambda printer, data: data, lambda data: True))
         elif (match.group(1) == 'F'):
             types += ['F']
             deep += 1
-            result.append((index, deep, 'F', match.group(2), select(match.group(2))))
+
+            iflambda = lambda data: True
+            if (not match.group(3) == None):
+                iflambda = createtester(match.group(3))
+
+            result.append((index, deep, 'F', match.group(2), select(match.group(2)), iflambda))
+        elif ((match.group(1) == 'A')or(match.group(1) == 'AGGREGATE')): #aggregate
+            params = [match.group(3)]
+            if (',' in params[0]):
+                params = match.group(3).split(',')
+            iflambda = lambda data: True
+            if (len(params) > 1):
+                iflambda = createtester(params[1])
+
+            result.append((index, deep, 'A', match.group(2), agregate(params[0], match.group(2))))
         elif (match.group(1) == 'I'):
             result.append((index, deep, 'P', match.group(2), softprinter(match.group(2))))
+        elif (match.group(1) == 'FILTER'):
+            params = [match.group(3)]
+            iflambda = createtester(params[0])
+            result.append((index, deep, 'FILTER', '', filterit(iflambda)))
+            pass
         elif (match.group(1) == 'E'):
             ltype = types[-1]
             #print(types)
@@ -108,15 +181,20 @@ def compile_template(template):
             #print('reversed sub', str(funclisttorun))
             deep -= 1
             if (ltype == 'W'):
-                result.append((index, deep, 'subroutine:', '', subroutine(funclisttorun)))
-            if (ltype == 'F'):
+                result.append((index, deep, 'subroutine:', '', subroutine(funclisttorun, sub[0][5])))
+                #result.append((index, deep, 'subroutine:', '', subroutine(funclisttorun)))
+            elif (ltype == 'F'):
                 wraped = [funclisttorun[0]]
-                wraped += [iterator(funclisttorun[1:])]
+                wraped += [iterator(funclisttorun[1:], sub[0][5])]
+                #wraped += [iterator(funclisttorun[1:])]
                 result.append((index, deep, 'subroutine:', '', subroutine(wraped)))
 
-    print(result)
+
+    for item in result:
+        print(item)
     funclisttorun = list(map(lambda item: item[4], result))
     if (not result[-1][1] == 0):
         print ('ERROR, [[E]] missing')
+        print (result[-2])
 
     return subroutine(funclisttorun)
