f8503c4355450343d31d902100e0918e145f40aa
hrbolek <33178647+hrbolek@users.noreply.github.com>
2019-06-05 21:16:06 +0200

    Update textanalyzer.py

--- source/textanalyzer.py
+++ source/textanalyzer.py
@@ -1,34 +1,34 @@
 import func2pipe as fp
 from functools import reduce
 
-@fp.pipeitwithnamedparams
+@fp.pipeit
 def replace(line, original, replacement):
     result = str(line).replace(original, replacement)
     return result
 
-@fp.pipeitwithnamedparams
+@fp.pipeit
 def intocolumns(line, separator):
     result = str(line).split(separator)
     return result
 
-@fp.pipeitwithnamedparams
+@fp.pipeit
 def intotext(cols, separator):
     result = reduce(lambda x, y: x + separator + y, cols.values())
     return result
 
-@fp.pipeitwithnamedparams
+@fp.pipeit
 def namecols(cols, names):
     result = {}
     for nn in zip(names, range(0, 100)):
         result[nn[0]] = cols[nn[1]]
     return result
 
-@fp.pipeitwithnamedparams
+@fp.pipeit
 def addcol(cols, name, func):
     result = { **cols, name: func(cols) }
     return result
 
-@fp.pipeitwithnamedparams
+@fp.pipeit
 def removecol(cols, name):
     result = { **cols }
     if name in result:
